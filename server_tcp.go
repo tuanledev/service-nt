92fe6a930a58b94b918924b5b8992b342fde9419
tuanlt <tuanlt21@fpt.com.vn>
2018-12-17 15:17:44 +0700

    .

--- server_tcp.go
+++ server_tcp.go
@@ -1,29 +1,81 @@
+/*
+A very simple TCP server written in Go.
+This is a toy project that I used to learn the fundamentals of writing
+Go code and doing some really basic network stuff.
+Maybe it will be fun for you to read. It's not meant to be
+particularly idiomatic, or well-written for that matter.
+*/
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+var addr = flag.String("addr", "", "The address to listen to; default is \"\" (all interfaces).")
+var port = flag.Int("port", 9000, "The port to listen on; default is 8000.")
 
 func main() {
+	flag.Parse()
 
-	fmt.Println("Launching server...")
+	fmt.Println("Starting server...")
 
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":9000")
+	src := *addr + ":" + strconv.Itoa(*port)
+	listener, _ := net.Listen("tcp", src)
+	fmt.Printf("Listening on %s.\n", src)
 
-	// accept connection on port
-	conn, _ := ln.Accept()
+	defer listener.Close()
 
-	// run loop forever (or until ctrl-c)
 	for {
-		// will listen for message to process ending in newline (\n)
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Some connection error: %s\n", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
+
+	scanner := bufio.NewScanner(conn)
+
+	for {
+		ok := scanner.Scan()
+		fmt.Println("Msg ", scanner.Text())
+		conn.Write([]byte(scanner.Text() + "\n"))
+		if !ok {
+			break
+		}
+	}
+	fmt.Println("Client at " + remoteAddr + " disconnected.")
+}
+
+func handleMessage(message string, conn net.Conn) {
+	fmt.Println("> " + message)
+	if len(message) > 0 && message[0] == '/' {
+		switch {
+		case message == "/time":
+			resp := "It is " + time.Now().String() + "\n"
+			fmt.Print("< " + resp)
+			conn.Write([]byte(resp))
+
+		case message == "/quit":
+			fmt.Println("Quitting.")
+			conn.Write([]byte("I'm shutting down now.\n"))
+			fmt.Println("< " + "%quit%")
+			conn.Write([]byte("%quit%\n"))
+			os.Exit(0)
+
+		default:
+			conn.Write([]byte("Unrecognized command.\n"))
+		}
 	}
 }
